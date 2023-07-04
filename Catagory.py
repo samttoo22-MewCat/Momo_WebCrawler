e048cfad4f0815d6c3d33ef8a1a7695fc102bc77
samttoo22 <v99sam@gmail.com>
2023-07-05 02:19:59 +0800

    added some comments

--- Catagory.py
+++ Catagory.py
@@ -27,14 +27,16 @@ class Category():
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--user-data-dir=C:\\Users\\v99sa\\Desktop\\coding\\py\\Momo_WebCrawler\\Momo_WebCrawler\\profile1")
             return options
-        
+    
+    #找出大分類    
     def getCatagories(self, type):
         catagories = self.driver.find_element(By.ID, "bt_cate_top")
         if(type == "webElement"):
             return catagories
         elif(type == "string"):
             return catagories.text
-        
+    
+    #找出小分類    
     def getRawLists(self):
         wait = WebDriverWait(self.driver, 20)
         
@@ -48,18 +50,24 @@ class Category():
                 
         rawLists = self.driver.find_elements(By.XPATH, "//table[@class = 'wrapTable']//tbody")
         return rawLists
+    
+    #找出小分類中的 "品牌"
     def getBrandList(self, catagory, type):
         wait = WebDriverWait(self.driver, 20)
         
+        #拿到類別的按鈕和連結
         catagoryBotton = self.getCatagories("webElement")
         catagoryBotton = catagoryBotton.find_element(By.XPATH, ".//a[contains(text(),'%s')]" % catagory)
         catagoryLink = catagoryBotton.get_attribute("href")
-        print(catagoryLink)
         
+        #轉到類別的連結，然後等待到類別下小分類顯示出來
         self.driver.get(catagoryLink)
         wait.until(EC.visibility_of_element_located((By.XPATH, "//th[contains(text(),'品牌')]")))
-                
+        
+        #小分類的按鈕列表
         brandListBottons = self.driver.find_elements(By.XPATH, "//table[@class = 'wrapTable']//tbody//tr//td//div[@class= 'wrapDiv']//ul[@id = 'brandsList']//li//label")
+        
+        #分成輸出元素還是純文字
         if(type == "webElement"):
             return brandListBottons
         elif(type == "string"):
