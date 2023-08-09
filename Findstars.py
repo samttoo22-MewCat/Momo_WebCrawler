f2d88a4a5d0984daf9397196c3a0c2c70351d6a4
samttoo22 <v99sam@gmail.com>
2023-08-09 10:52:35 +0800

    json completed

--- Findstars.py
+++ Findstars.py
@@ -9,25 +9,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime, timedelta
 
 class FindStar: 
-    def __init__(self) -> None:
-        self.driver = uc.Chrome(browser_executable_path=r"C:\\Users\\v99sa\\Desktop\\chrome-win\\chrome.exe", options=self.__get_ChromeOptions(), version_main=110)
-        self.driver.get('https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=3959576&recomd_id=hotSale&cid=recitri&oid=BfG&mdiv=&ctype=B')
-        self.wait = WebDriverWait(self.driver, 20)
-    def __get_ChromeOptions(self): 
-        options = uc.ChromeOptions()
-        options.add_argument('--start_maximized')
-        options.add_argument("--disable-extensions")
-        options.add_argument('--disable-application-cache')
-        options.add_argument('--disable-gpu')
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-notifications")
-        options.add_argument("--incognito")
+    def __init__(self, driver) -> None:
+        self.driver = driver
         
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--user-data-dir=C:\\Users\\v99sa\\Desktop\\coding\\py\\Momo_WebCrawler\\Momo_WebCrawler\\profile1")
-        return options
+        self.wait = WebDriverWait(self.driver, 20)
 
     def findProduct(self):
+        self.driver.get('https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=3959576&recomd_id=hotSale&cid=recitri&oid=BfG&mdiv=&ctype=B')
         # 評論數
         comments = self.driver.find_element(By.XPATH, "//li[@class = 'goodsCommendLi']")
         comments.click()
@@ -37,6 +25,3 @@ class FindStar:
         #print(comments.get_attribute("outerHTML"))
         print(star.get_attribute("textContent"))
         print(comments.text)
-
-f = FindStar()
-f.findProduct() 
--- Product.py
+++ Product.py
@@ -1,25 +1,31 @@
+import requests
 from selenium import webdriver
+# -*- coding: UTF-8 -*-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
+from datetime import datetime, timedelta
 import json
 import gc
-import requests
+import pymysql
 
-class Product():
-    def __init__(self) -> None:
-        self.driver = uc.Chrome(browser_executable_path=r"C:\\Users\\v99sa\\Desktop\\chrome-win\\chrome.exe", options=self.__get_ChromeOptions(), version_main=110)
-        self.driver.get('https://www.momoshop.com.tw/category/LgrpCategory.jsp?l_code=1111700000&sourcePageType=4')
+class Product(object):
+    def __init__(self, driver, useragent) -> None:
+        self.driver = driver
+        self._useragent = useragent
         
-        self.url = 'https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=9007774&recomd_id=hotSale&cid=recitri&oid=BfG&mdiv=&ctype=B'
+        
+        self.wait = WebDriverWait(self.driver, 20)
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+            'User-Agent': self._useragent
         }
-        self.res = requests.post(self.url, headers=self.headers)
-        self.res.encoding = 'uth-8'
-        self.soup = BeautifulSoup(self.res.text,"lxml")
-    
+
+        
+
     def __get_ChromeOptions(self): 
             options = uc.ChromeOptions()
             options.add_argument('--start_maximized')
@@ -29,43 +35,96 @@ class Product():
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-notifications")
             options.add_argument("--incognito")
-            
             options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--user-data-dir=C:\\Users\\v99sa\\Desktop\\coding\\py\\Momo_WebCrawler\\Momo_WebCrawler\\profile1")
             return options
-
-    def getProductInfo(self):
+    def returnURL(self):
+        return self._url
+    def getProductInfo(self, url):
+        self.driver.get(url)
+        pageSource = self.driver.page_source.encode("utf-8")
+        soup = BeautifulSoup(pageSource,'lxml')
         # 商品名稱
-        name = self.soup.find(id = 'osmGoodsName').text
-        print(name)
+        name = str(soup.find(id = 'osmGoodsName').text)
         # 最折扣價格
-        price = self.soup.find("meta", property="product:price:amount")
-        print(price["content"] if price else "No meta price given")
+        price = soup.find("meta", property="product:price:amount")
         # 品牌名稱
-        brand = self.soup.find("meta", property="product:brand")
-        print(brand["content"] if brand else "No meta brnad given")
+        brand = soup.find("meta", property="product:brand")
         # 品號
-        productid = self.soup.find("meta", property="product:retailer_item_id")
-        print(productid["content"] if productid else "No meta product_id given")
-
-        # 銷售量
-        # attributeArea = soup.find('div', {'class': 'productRatingFlex'})
-        # print(attributeArea)
-        # 評價數
-        # 評價星號
-
-        table = self.soup.find('div', {'class': 'attributesArea'}).find('table')
+        productid = soup.find("meta", property="product:retailer_item_id")
+        
+        # 商品規格表格讀取
+        table = soup.find('div', {'class': 'attributesArea'}).find('table')
         columns =  [tr for tr in table.findAll('tr')]
         tds = [ column.find("td") for column in columns ]
+        ths = [ column.find("th").text for column in columns ]
+
+        # 商品規格 - 品牌系列名稱
+        seriesNum = ths.index("品牌系列名稱") if "品牌系列名稱" in ths else -1
+        if seriesNum == -1 :
+            series = None
+        else:
+            series = tds[seriesNum].text
         # 商品規格 - 品牌定位
-        brandtype = tds[1].text
-        print(brandtype)
+        brandtypeNum = ths.index("品牌定位") if "品牌定位" in ths else -1
+        if  brandtypeNum == -1 :
+            brandtype = None
+        else:
+           brandtype = tds[brandtypeNum].text
         # 商品規格 - 包裝組合
-        package =  tds[2].text
-        print(package)
+        packageNum = ths.index("包裝組合") if "品牌定位" in ths else -1
+        if  packageNum == -1 :
+            package = None
+        else:
+            package =  str(tds[packageNum]).replace('<td>',"").replace('<ul>',"").replace('<li>',"").replace('</li>',"*").replace('</ul>',"").replace('</td>',"")
         # 商品規格 - 功效
-        function =  str(tds[3]).replace('<td>',"").replace('<ul>',"").replace('<li>',"").replace('</li>',"*").replace('</ul>',"").replace('</td>',"")
-        print(function)
+        functionNum = ths.index("功效") if "功效" in ths else -1
+        if  functionNum == -1 :
+            function= None
+        else:
+            function = str(tds[functionNum]).replace('<td>',"").replace('<ul>',"").replace('<li>',"").replace('</li>',"*").replace('</ul>',"").replace('</td>',"")
         # 商品規格 - 適用於
-        usage =  tds[4].text
-        print(usage)
+        usageNum = ths.index("適用於") if "適用於" in ths else -1
+        if  usageNum == -1 :
+            usage = None
+        else:
+            usage = str(tds[usageNum]).replace('<td>',"").replace('<ul>',"").replace('<li>',"").replace('</li>',"*").replace('</ul>',"").replace('</td>',"")
+        # 銷售量
+        #self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='productRatingFlex']/p")))
+        try:
+            sales =  self.driver.find_element(By.XPATH, "//*[@id='productRatingFlex']/p") 
+            sales = sales.get_attribute("textContent")
+        except:
+            sales = "0"
+        # 評論數
+        comments = self.driver.find_element(By.XPATH, "//li[@class = 'goodsCommendLi']")
+        
+        if comments.text == "商品評價(0)" :
+             starCount = 0 
+        else:
+            comments.click()
+        # 總星星數
+        star = self.driver.find_element(By.XPATH, "//div[@class = 'indicatorAvg']//div[@class = 'indicatorAvgVal']")
+        starCount = star.get_attribute("textContent")
+        comments = str(comments.text).replace('商品評價(','').replace(')','')
+
+        ProductDict = {
+            "name": name,
+            "id": productid["content"], 
+            "price":price["content"],
+            "brand": brand["content"],
+            "brandtype": brandtype,
+            "series":series,
+            "package":package, 
+            "function":function, 
+            "usage":usage, 
+            "sales":sales,
+            "comments":comments, 
+            "star":starCount
+        }
+        return ProductDict
+    
+#p = Product("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",'https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=9007774&recomd_id=hotSale&cid=recitri&oid=BfG&mdiv=&ctype=B', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
+#json_object = p.getProductInfo() 
+#json_formatted_str = json.dumps(json_object, ensure_ascii = False, indent=2)
+#print(json_formatted_str)
+
